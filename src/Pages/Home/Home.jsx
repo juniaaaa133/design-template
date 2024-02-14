3712b3e506474dfdb5c465ca370abf3fb519b85e
Reinnn <xopeofhopeness2004@gmail.com>
2024-02-14 17:04:40 +0630

    Fixed animation error

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -25,8 +25,8 @@ let hero_button_text = "Contact Us Now";
   <img src={require('../../img/layout2.png')} alt="" className='layout2'/>
   <img src={require('../../img/layout2.png')} alt="" className='layouth3'/>
   <motion.div 
-   initial={{ opacity: 0,  translateY : 200 }}
-   animate={{ opacity: 1, translateY : 0}}
+   initial={{ opacity: 0,  }}
+   animate={{ opacity: 1, }}
      transition={{ delay: 0,duration : 0.6}}
    >  <div className="hm-main-div">
     <div className="hm-title-ctn">
